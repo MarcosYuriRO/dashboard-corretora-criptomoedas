f8ca3b072b8d98f93fcaf2961a4f00c091c0d87f
Marcos <marcos.yuriro@gmail.com>
2026-07-01 17:00:26 -0300

    build: define submodule in dashboard module

---
+++ criptomoedas/src/main/java/com/coinlovers/criptomoedas/CriptomoedasApplication.java
@@ -0,0 +1,13 @@
+package com.coinlovers.criptomoedas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriptomoedasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriptomoedasApplication.class, args);
+	}
+
+}
